ac2c5f6589e2eda8a4999f4fa51a4c007225796a
Ginger Owen <gingerowen0421@gmail.com>
2022-10-10 12:21:17 -0600

    changed vars

--- Develop/script.js
+++ Develop/script.js
@@ -1,39 +1,48 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  var passwordLength = 
+//assign each potential part of the password to a variable
+var upperCase
+var lowerCase
+var specialCharacters
+var numbers
+var length 
 
-  passwordText.value = password;
 
+function charPrompt (params) {
+  upperCase = confirm("Has uppercase characters?");
+  lowerCase = confirm("Has lowercase letters?");
+  specialCharacters = confirm("Has special characters?");
+  numbers = confirm("Has numbers?");
+  length = prompt("How long do you want your password to be?");
+  console.log(upperCase, lowerCase, numbers, specialCharacters, length);
 }
+// Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+//   var passwordLength = 
+
+//   passwordText.value = password;
+
+// }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-//what I've done is below this line
-//define characters that password can contain
+//variables for the password
 
-const lettersLowerCase = "abcdefghijklmnopqrstuvwxyz";
+// var lowerCase = ["Include lower case letters?",  
+// "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-const lettersUpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+ 
+// var upperCase = prompt("Include upper case letters?",
+// "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
 
-const specialCharacters = "!@#$%^&*-_=";
+// var specialCharacters = prompt("Include special characters?", 
+// "!@#$%^&*-_=")
 
-const numbers = "1234567890";
+// var numbers = prompt("Include numbers?", "1234567890")
 
-//first argument propmt label that displays textbox 
-
-var lowerCase = prompt("Include lower case letters?",  
-"abcdefghijklmnopqrstuvwxyz");
-
-var upperCase = prompt("Include upper case letters?",
-"ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-
-var specialCharacters = prompt("Include special characters?", 
-"!@#$%^&*-_=")
-
-var numbers = prompt("Include numbers?", "1234567890")
+// var index = Math.floor(Math.random() * lowerCase.length);
+//   var computerChoice = options[index];
