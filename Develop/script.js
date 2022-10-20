578d773a5c482002493ec95150982e5405d0c518
Ginger Owen <gingerowen0421@gmail.com>
2022-10-20 14:46:30 -0600

    generates password

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+var passwordText = document.getElementById("#password");
+var password = "";
 //assign each potential part of the password to a variable
 var upperCase;
 var lowerCase;
@@ -8,13 +9,14 @@ var specialCharacters;
 var numbers;
 var userLength;
 
+
 function charPrompt (params) {
   upperCase = confirm("Has uppercase characters?");
   lowerCase = confirm("Has lowercase letters?");
   specialCharacters = confirm("Has special characters?");
   numbers = confirm("Has numbers?");
   userLength = parseInt(prompt("How long do you want your password to be?"));
-  console.log(upperCase, lowerCase, numbers, specialCharacters, userLength);
+  //console.log(upperCase, lowerCase, numbers, specialCharacters, userLength);
 
   if (isNaN(userLength)===true || userLength < 8 || userLength > 128) {
     alert("password length is not valid")
@@ -22,47 +24,52 @@ function charPrompt (params) {
   }
 };
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", charPrompt);
-
-function generatePassword() {
   let nums = "1234567890"
   let ucLetters = "QWERTYUIOPASDFGHJKLZXCVBNM"
   let lcLetters = "qwertyuioplkjhgfdsazxcvbnm"
   let special = "!@#$%^&*()"
-  let userChoice = " "
-  let password = " "
+  let userChoice = ""
 
-  console.log(nums, ucLetters, lcLetters, special, userChoice)
+function generatePassword() {
 
-  if (upperCase=true) {
-    userChoice+=ucLetters;
-    console.log(userChoice)
-  }
-  if (lowerCase=true) {
-    userChoice+=lcLetters
-  }
-  if (specialCharacters=true) {
-    userChoice+=special
-  }
-  if (numbers=true) {
-    userChoice+=nums
-  }
-  console.log(userChoice)
 
-  for (let i = 0; i < length; i++) {
+  //console.log(nums, ucLetters, lcLetters, special, userChoice)
+
+  if (upperCase) {
+    userChoice += ucLetters;
+    //console.log(userChoice)
+  }
+  if (lowerCase) {
+    userChoice += lcLetters
+  }
+  if (specialCharacters) {
+    userChoice += special
+  }
+  if (numbers) {
+    userChoice += nums
+  }
+  //console.log(userChoice)
+
+  for (let i = 0; i < userLength; i++) {
     let randomIndex = Math.floor(Math.random() * userChoice.length)
     
     let randomCharacter = userChoice[randomIndex]
 
-    password = password+=randomCharacter
+    password = password + randomCharacter;
   }
 
   console.log(password)
+
+  return password
+  
+  
 };
 
-generatePassword();
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", () => {
+  charPrompt()
+  generatePassword()
+  document.getElementById("password").textContent = password;
+});
 
-// var index = Math.floor(Math.random() * lowerCase.length);
-//   var computerChoice = options[index];
