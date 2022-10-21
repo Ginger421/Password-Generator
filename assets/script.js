e9888abcfc25eb12d28f893b07bb67462872657f
Ginger Owen <gingerowen0421@gmail.com>
2022-10-20 22:15:20 -0600

    added comments

--- assets/script.js
+++ assets/script.js
@@ -3,37 +3,37 @@
 var generateBtn = document.querySelector("#generate");
 var passwordText = document.getElementById("#password");
 var password = "";
-//assign each potential part of the password to a variable
+//assign each set of characters to a variable that the users picks from
 var upperCase;
 var lowerCase;
 var specialCharacters;
 var numbers;
 var userLength;
 
-
+//ask the users which characters to include in password
 function charPrompt (params) {
   upperCase = confirm("Has uppercase characters?");
   lowerCase = confirm("Has lowercase letters?");
   specialCharacters = confirm("Has special characters?");
   numbers = confirm("Has numbers?");
   userLength = parseInt(prompt("How long do you want your password to be?"));
-  //console.log(upperCase, lowerCase, numbers, specialCharacters, userLength);
 
+  //check to make sure user length is a numebr
   if (isNaN(userLength)===true || userLength < 8 || userLength > 128) {
     alert("password length is not valid")
     charPrompt()
   }
 };
 
+//all varaibles that represent potential characters to be included in password
   let nums = "1234567890"
   let ucLetters = "QWERTYUIOPASDFGHJKLZXCVBNM"
   let lcLetters = "qwertyuioplkjhgfdsazxcvbnm"
   let special = "!@#$%^&*()"
   let userChoice = ""
 
+  //function that generates password and puts parts together
 function generatePassword() {
-
-
   //console.log(nums, ucLetters, lcLetters, special, userChoice)
 
   if (upperCase) {
@@ -67,7 +67,7 @@ function generatePassword() {
 };
 
 
-// Add event listener to generate button
+// Add event listener to generate button when clicked call prompts and generatepassword functions 
 generateBtn.addEventListener("click", () => {
   charPrompt()
   generatePassword()
